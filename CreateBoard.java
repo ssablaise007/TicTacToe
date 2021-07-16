29ec348913f35ffb7b7910070b8b37d7d4b4d517
Your Name <you@example.com>
2021-07-16 09:25:37 +0530

    Added : Creating Char with Size[10]

---
+++ CreateBoard.java
@@ -0,0 +1,16 @@
+package TicTacToe;
+
+public class CreateBoard {
+
+	public static void CreateBoard() {
+		char[] Board= new char[5];
+		Board[1] = "";
+		Board[2] = "";
+		Board[3] = "";
+		Board[4] = "";
+	}
+	
+	public static void main(String args[]) {
+		CreateBoard();
+	}
+}
